9dfd3ca2b408b05e351688a76b27342391a95b69
Serena Wong <serenaw@stanford.edu>
2015-07-19 20:23:46 +0200

    update .rb

--- switch_app.rb
+++ switch_app.rb
@@ -1,21 +1,21 @@
 require "sinatra"
 
 get "/" do 
-
+  erb :home_page
 end
 
 get "/pushups" do 
-  
+  erb :pushups  
 end
 
 get "/squats" do 
-  
+    erb :squats
 end
 
 get "/treadmill" do 
-  
+    erb :treadmill
 end
 
 get "/trainers" do 
-  
+    erb :trainers
 end
